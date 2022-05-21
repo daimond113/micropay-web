bad102231ffac5fd5b6870a0ebc7865ca4afeb1c
dai <72147841+daimond113@users.noreply.github.com>
2022-05-21 15:59:23 +0200

    chore: fix ts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
+import React from 'react'
 
-function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }: { Component: () => JSX.Element, pageProps: AppProps['pageProps'] }) {
   return <SessionProvider session={session}>
     <Component {...pageProps} />
   </SessionProvider>
